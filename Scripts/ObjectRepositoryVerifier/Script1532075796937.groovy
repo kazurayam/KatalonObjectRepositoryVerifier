2e88f47a41b7b72e24c80d604f1d7a5498725bab
kazurayam <kazuaki.urayama@gmail.com>
2018-07-20 23:14:43 +0900

    updates

--- Scripts/ObjectRepositoryVerifier/Script1532075796937.groovy
+++ Scripts/ObjectRepositoryVerifier/Script1532075796937.groovy
@@ -12,18 +12,43 @@ import javax.xml.xpath.XPathFactory
 import org.w3c.dom.Document
 
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.util.KeywordUtil
 
+
+KeywordLogger logger = new KeywordLogger()
 
 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance()
 XPath xpath = XPathFactory.newInstance().newXPath()
 
+List<String> expressions = [
+	'/WebElementEntity/webElementProperties[name="tag" and isSelected="true"]',
+	'/WebElementEntity/webElementProperties[name="class" and isSelected="true"]'
+]
+
 Path objectRepository = Paths.get('Object Repository')
 List<Path> rsFiles = Files.walk(objectRepository)
-	.filter({p -> Files.isRegularFile(p)})
+	.filter {p -> Files.isRegularFile(p)}
+	.filter {p -> p.toString().endsWith('.rs')}
 	.collect(Collectors.toList())
+
+def failureCount = 0
 for (Path p : rsFiles) {
 	DocumentBuilder db = dbf.newDocumentBuilder()
 	Document doc = db.parse(p.toFile())
 	String name = (String)xpath.evaluate("/WebElementEntity/name", doc, XPathConstants.STRING)
-	WebUI.comment("name=${name}")
+	Boolean isBasic = (Boolean)xpath.evaluate('/WebElementEntity/selectorMethod[. = "BASIC"]', doc, XPathConstants.BOOLEAN)
+	if (isBasic) {
+		for (String expr : expressions) {
+			Boolean result = (Boolean)xpath.evaluate(expr, doc, XPathConstants.BOOLEAN)
+			if (!result) {
+				logger.logFailed("FAILURE ${p} against XPath \"${expr}\"")
+				failureCount += 1	
+			}
+		}
+	}
+}
+
+if (failureCount > 0) {
+	KeywordUtil.markFailed("${failureCount} failures")
 }
